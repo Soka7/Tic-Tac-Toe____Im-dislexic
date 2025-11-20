c2d81c62a16207f2bae1d1a9aa7aec602237846e
jesus-dev7 <kids.anguita@gmail.com>
2025-11-20 07:34:26 +0100

    Chrono

---
+++ Chrono.py
@@ -0,0 +1,37 @@
+import time
+import threading as th
+
+class Chrono:
+    def __init__(self):
+        self.Signal = False
+    
+    def StopTime(self, Time, Timer = False):
+        """Stop time for Time seconds.
+
+        Args:
+            Time (int): time
+        """
+        time.sleep(Time)
+        if Timer == True:
+            self.Signal = True
+        return None
+        
+    def GetTimeFunction(self, Fonction, *args, **kwargs):
+        Start = time.time()
+        Fonction(*args, **kwargs)
+        End = time.time()
+        Exe = End - Start
+        
+        return Exe
+    
+    def SetTimer(self, Time):
+        assert Time < 600, "Too long"
+        print(f"Waiting {Time} seconds.")
+        Thread = th.Thread(target = self.StopTime(Time, True))
+        while True:
+            if self.Signal == True:
+                self.Signal = False
+                break
+        return True
+        
+Chrono1 = Chrono()
