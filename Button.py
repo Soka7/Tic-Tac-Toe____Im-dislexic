1116d89b8dda5a734dceb1c53ae83e007a9a9052
Yolked64 <leobodinleo@gmail.com>
2025-11-22 19:19:09 +0100

    Layout
    Added the buttons of the UI and started stylizing it a bit.
    To do list at the top of the UI file

--- Button.py
+++ Button.py
@@ -2,26 +2,71 @@ from tkinter import *
 import tkinter as tk
 
 class BetterButton:
-    def __init__(self, root : tk) -> None:
+    def __init__(self, root : tk, Text : str) -> None:
         """
-        Create a button with the biding already done when the mouse hover it.
+        Create a button using root argument for the root of the button.
+        Along with the text you want the button to display.
         """
-        self.NewButton : Button = Button(root, text = "I got created")
+
+        assert type(Text) == str, "The text argument must be a string."
+
+        self.NewButton : Button = Button(root, text = Text)
         return None
     
     def Enhance(self, UsedColorActive : str, UsedFontActive : tuple, UsedColorIdle : str, UsedFontIdle : tuple) -> None:
         """
         Enhance the button by giving its bidings.
+        Used colors must be string hexacode.
+        Used fonts must be tuple where the first element is the font as a string and second is the size as an integer.
         """
+
+        assert type(UsedColorActive) == str and type(UsedColorIdle) == str, "The used color must be an hexacode as a string date type."
+        assert type(UsedFontActive) == tuple and type(UsedFontIdle) == tuple, "The used font must be a tuple."
+
         self.NewButton.bind('<Enter>', lambda e : self._OnHover(e, UsedColorActive, UsedFontActive))
         self.NewButton.bind('<Leave>', lambda e : self._OnDefault(e, UsedColorIdle, UsedFontIdle))
         return None
     
+    def SetAppearence(self, Background : str, Foreground : str, Border : int) -> None:
+        """
+        Change the appearance of the button.
+        Background and Foreground being the background and foregroud of the button, both must be a str hexacode.
+        """
+
+        assert type(Background) == str and type(Foreground) == str, "Background and Foreground must be hexacode as strings."
+
+        self.NewButton.config(background = Background,
+                              foreground = Foreground,
+                              border = Border)
+        return None
+    
+    def SetSize(self, width : int, height : int) -> None:
+        """
+        Set the size of the button.
+        The width and height parameters must be integers.
+        """
+        assert type(width) == int and type(height) == int, "The height and width must be integers."
+
+        self.NewButton.config(width = width, height = height)
+        return None
+    
     def PlaceButton(self, PosX : int, PosY : int, anchor : str) -> None:
         """
-        Place a button.
+        Place a button at the PosX x position and PosY y position.
+        Along with the anchor being one of the base anchor of tkinter, as a string.
         """
+
+        assert type(PosX) == int and type(PosY) == int, "The coordinates must be integers."
+        assert type(anchor) == str, "The anchor has to be a string, and one of tkinter's anchor."
+
         self.NewButton.place(x = PosX, y = PosY, anchor = anchor)
+        return None
+
+    def HideButton(self) -> None:
+        """
+        Hide the button using the place_forget() method.
+        """
+        self.NewButton.place_forget()
         return None
 
     def _OnHover(self, event, UsedColor : str, UsedFont : tuple) -> None:
--- Ui.py
+++ Ui.py
@@ -4,23 +4,88 @@ import tkinter as tk
 from StackFile import Stack
 from Button import BetterButton
 
+# Reminder to install Nunito (font) and use it on Linux
+# Maybe a Class text allowing to easily choose how to place label or just use label, idk
+
 class Ui:
     def __init__(self) -> None:
         """
         Create the UI.
         """
-        self.root = tk.Tk()
+        # Creating window
+        self.Root = tk.Tk()
+        self.Canvas = tk.Canvas()
         self.CurrentMenu = Stack()
+        self.WindowWidth : int = 1200
+        self.WindowHeight: int = 760
 
-        self.root.geometry('1200x760')
-        self.root.title("Helper")
+        # Buttons creation
+        self.RulesButton : BetterButton = BetterButton(self.Root, "Rules")
+        self.InputButton : BetterButton = BetterButton(self.Root, "Inputs Map")
+        self.ReqButton : BetterButton = BetterButton(self.Root, "Requirements")
+        self.TryButton : BetterButton = BetterButton(self.Root, "Demo")
+        self.CreditButton : BetterButton = BetterButton(self.Root, "Credits")
+
+        # Setting the size of the buttons
+        self.RulesButton.SetSize(20, 4)
+        self.InputButton.SetSize(20, 4)
+        self.ReqButton.SetSize(20, 4)
+        self.TryButton.SetSize(20, 4)
+        self.CreditButton.SetSize(20, 4)
+
+        # Setting the Style of the buttons
+        self.RulesButton.SetAppearence("#1A1A1A", "#FFFFFF", 10)
+        self.InputButton.SetAppearence("#1A1A1A", "#FFFFFF", 10)
+        self.ReqButton.SetAppearence("#1A1A1A", "#FFFFFF", 10)
+        self.TryButton.SetAppearence("#1A1A1A", "#FFFFFF", 10)
+        self.CreditButton.SetAppearence("#1A1A1A", "#FFFFFF", 10)
+
+        # Root configuration
+        self.Root.geometry('1200x760')
+        self.Root.title("Helper")
+        self.Root.config(background = "#212121")
+
+        # Canvas configuration
+        self.Canvas.config(height = 80,
+                           width = 1200,
+                           background = "#161616",
+                           highlightbackground = "#1A1A1A")
+        return None
+    
+    def _ResizeWindow(self, width : int, height : int) -> None:
+        """
+        Resize the window with a specified width and height.
+        The window will appear at center of the screen.
+        """
+        ScreenWidth : int = self.Root.winfo_screenwidth()
+        ScreenHeight : int = self.Root.winfo_screenheight()
+
+        WindowX : float = ScreenWidth / 2 - width / 2
+        WindowY : float = ScreenHeight / 2 - height / 2
+
+        self.Root.geometry("%dx%d+%d+%d" % (width, height, WindowX, WindowY))
+
+        return None
+    
+    def _PlaceAllButtons(self) -> None:
+        """
+        Place all the buttons.
+        """
+        self.RulesButton.PlaceButton(int (0.16 * self.WindowWidth / 5), 0, "nw")
+        self.InputButton.PlaceButton(int (1.16 * self.WindowWidth / 5), 0, "nw")
+        self.ReqButton.PlaceButton(int (2.16 * self.WindowWidth / 5), 0, "nw" )
+        self.TryButton.PlaceButton(int (3.16 * self.WindowWidth / 5), 0, "nw")
+        self.CreditButton.PlaceButton(int (4.16 * self.WindowWidth / 5), 0, "nw")
         return None
 
     def ShowUi(self) -> None:
         """
         Display and place the UI on the screen.
         """
-        self.root.mainloop()
+        self._ResizeWindow(1200, 760)
+        self._PlaceAllButtons()
+        self.Canvas.pack()
+        self.Root.mainloop()
         return None
     
 ui = Ui()
