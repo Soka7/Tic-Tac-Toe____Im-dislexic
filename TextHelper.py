1f5000a071b1594ffaa073e5b3876c7868ec135b
Yolked64 <leobodinleo@gmail.com>
2025-11-23 19:06:41 +0100

    Continued Design
    Added more design
    Better code architecture

--- TextHelper.py
+++ TextHelper.py
@@ -2,13 +2,13 @@ from tkinter import *
 import tkinter as tk
 
 class BetterText:
-    def __init__(self, Root : tk, Text : str) -> None:
+    def __init__(self, Text : str) -> None:
         """
         Initialse the label by giving its root.
         """
         assert type(Text) == str, "The text must be a string."
         
-        self.NewLabel : Label = Label(root = Root)
+        self.NewLabel : Label = Label(text = Text)
         return None
 
     def EditText(self, Width : int, Height : int, Wraplenght : int = 0) -> None:
@@ -21,15 +21,38 @@ class BetterText:
         self.NewLabel.config(width = Width,
                              height = Height,
                              wraplength = Wraplenght)
+        
+    def DesignText(self, Background : str, Foreground : str, Font : tuple) -> None:
+        """
+        Apply a specified background and foreground to the text along with a font.
+        Background and Foreground must be hexacode strings.
+        Font must be a tuple containing the font and its size.
+        """
+        self.NewLabel.config(background = Background,
+                             foreground = Foreground,
+                             font = Font)
+        return None
 
-    def PlaceText(self, PosX : int, PosY : int, Anchor : str = "center") -> None:
+    def PlaceText(self, PosX : int, PosY : int, Anchor : str = "center", Relative : bool = False) -> None:
         """
         Place the text at the given coordinates.
         PosX and PosY must be integers and the anchor has to be s string.
+        Relative being a toggle to set relative position. Default is False.
         """
-
-        assert type(PosX) == int and type(PosY) == int, "Coordinates must be integers."
+        assert type(Relative) == bool, "Relative must be a boolean I.e True or False."
         assert type(Anchor) == str, "The anchor must be a string and from Tkinter's anchor."
+        if Relative:
+            assert type(PosX) == float and type(PosY) == float, "The relative size must be a float from 0 to 1."
+            self.NewLabel.place(relx = PosX, rely = PosY, anchor = Anchor)
+        else:
+            assert type(PosX) == int and type(PosY) == int, "Coordinates must be integers."
+            self.NewLabel.place(x = PosX, y = PosY, anchor = Anchor)
 
-        self.NewLabel.place(x = PosX, y = PosY, anchor = Anchor)
+        return None
+    
+    def HideText(self) -> None:
+        """
+        Hide the text using the place_forget() method of tkinter.
+        """
+        self.NewLabel.place_forget()
         return None
--- Ui.py
+++ Ui.py
@@ -3,6 +3,7 @@ import tkinter as tk
 
 from StackFile import Stack
 from Button import BetterButton
+from TextHelper import BetterText
 
 # Reminder to install Nunito (font) and use it on Linux
 # Maybe a Class text allowing to easily choose how to place label or just use label, idk
@@ -26,19 +27,17 @@ class Ui:
         self.TryButton : BetterButton = BetterButton(self.Root, "Demo")
         self.CreditButton : BetterButton = BetterButton(self.Root, "Credits")
 
-        # Setting the size of the buttons
-        self.RulesButton.SetSize(20, 4)
-        self.InputButton.SetSize(20, 4)
-        self.ReqButton.SetSize(20, 4)
-        self.TryButton.SetSize(20, 4)
-        self.CreditButton.SetSize(20, 4)
-
-        # Setting the Style of the buttons
-        self.RulesButton.SetAppearence("#1A1A1A", "#FFFFFF", 10)
-        self.InputButton.SetAppearence("#1A1A1A", "#FFFFFF", 10)
-        self.ReqButton.SetAppearence("#1A1A1A", "#FFFFFF", 10)
-        self.TryButton.SetAppearence("#1A1A1A", "#FFFFFF", 10)
-        self.CreditButton.SetAppearence("#1A1A1A", "#FFFFFF", 10)
+        # Labels creation
+        self.RulesTitle : BetterText = BetterText("Rules")
+        self.RulesText : BetterText = BetterText("1. Rock beats Lizard and Scissors, but get beaten by Paper and Spock.\n\n"
+                                                 "2. Lizard beats Spock and Paper, but get beaten by Scissors and Rock.\n\n"
+                                                 "3. Spock beats Rock and Scissors, but get beaten by Paper and Lizard.\n\n"
+                                                 "4. Scissors beats Rock and Lizard, but get beaten by Rock and Spock.\n\n"
+                                                 "5. Paper beats Rock and Spock, but get beaten by Scissors and Lizard.")
+        self.InputTitle : BetterText = BetterText("Input Map")
+        self.ReqTitle : BetterText = BetterText("Requirements")
+        self.TryTitle : BetterText = BetterText("Wanna try ?")
+        self.CreditsTitle : BetterText = BetterText("Credits")
 
         # Root configuration
         self.Root.geometry('1200x760')
@@ -51,6 +50,37 @@ class Ui:
                            background = "#141414",
                            highlightbackground = "#141414")
         return None
+    
+    def _ShowRules(self) -> None:
+        """
+        Show the rules.
+        """
+        self.RulesText.PlaceText(0.5, 0.5, "center", True)
+        return None
+
+    def _ResizeWidgets(self) -> None:
+        """
+        Configure teh size of all the widgets of the UI.
+        """
+        self.RulesButton.SetSize(20, 4)
+        self.InputButton.SetSize(20, 4)
+        self.ReqButton.SetSize(20, 4)
+        self.TryButton.SetSize(20, 4)
+        self.CreditButton.SetSize(20, 4)
+        return None
+    
+    def _DesignWidgets(self) -> None:
+        """
+        Apply a defined style to all widgets.
+        """
+        #self.RulesText.DesignText("#1A1A1A", "#FFFFFF")
+        self.RulesButton.SetAppearence("#1A1A1A", "#FFFFFF", 10)
+        self.InputButton.SetAppearence("#1A1A1A", "#FFFFFF", 10)
+        self.ReqButton.SetAppearence("#1A1A1A", "#FFFFFF", 10)
+        self.TryButton.SetAppearence("#1A1A1A", "#FFFFFF", 10)
+        self.CreditButton.SetAppearence("#1A1A1A", "#FFFFFF", 10)
+        return None
+
     
     def _ResizeWindow(self, Width : int, Height : int) -> None:
         """
@@ -67,9 +97,9 @@ class Ui:
 
         return None
     
-    def _PlaceAllButtons(self) -> None:
+    def _PlaceWidgets(self) -> None:
         """
-        Place all the buttons.
+        Place all the widgets
         """
         self.RulesButton.PlaceButton(int (0.16 * self.WindowWidth / 5), 0, "nw")
         self.InputButton.PlaceButton(int (1.16 * self.WindowWidth / 5), 0, "nw")
@@ -83,7 +113,10 @@ class Ui:
         Display and place the UI on the screen.
         """
         self._ResizeWindow(1200, 760)
-        self._PlaceAllButtons()
+        self._ResizeWidgets()
+        self._DesignWidgets()
+        self._PlaceWidgets()
+        self._ShowRules()
         self.Canvas.pack()
         self.Root.mainloop()
         return None
