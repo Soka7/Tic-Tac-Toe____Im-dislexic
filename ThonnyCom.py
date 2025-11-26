225200b7235dbedf9e64a2981a59caec3da8ec1e
Yolked64 <leobodinleo@gmail.com>
2025-11-26 13:23:49 +0100

    Last Fix
    QoL, made the communication file as a class
    Changed the way input is sent to send it everytime a button is pressed if 5 buttons were pressed already.

--- ThonnyCom.py
+++ ThonnyCom.py
@@ -1,12 +1,12 @@
 from microbit import *
 
-Input : str = "" # String stocking the player inputs
+Input : str = "" # String stocking the player inputs.
 
 while True:
-    if button_a.was_pressed(): # if the player pressed a, add A
+    if button_a.was_pressed(): # If the player pressed a, add A.
         Input += "A"
-    if button_b.was_pressed(): # if the player pressed n, add B
+    if button_b.was_pressed(): # If the player pressed b, add B.
         Input += "B"
     if len(Input) == 5: # Since the combination for the UI is 5 letters long
         print(Input)    # When it's 5 letter long, we print it, it gets read with serial
-        Input = ""      # if it's the right one the UI shows, and we reset the combination
+        Input = Input[1:] # When the input is 5 letters long, replace it with the 5 last letters.
--- communication.py
+++ communication.py
@@ -1,11 +1,26 @@
 import serial
 from Ui import Ui
 
-ser = serial.Serial('/dev/ttyACM0', 115200, timeout=1) # Create the connection between Card and Pc
-
-print("Reading from micro:bit...") # Debug message
-
-ui = None
+class Com:
+    def __init__(self) -> None:
+        """
+        Create the class by creating the connection with the card and pc.
+        """
+        self.ser = serial.Serial('/dev/ttyACM0', 115200, timeout=1) # Create the connection between Card and Pc
+        self.ui = None
+        print("Reading from micro:bit...") # Debug message
+        return None
+    
+    def SpawnUi(self) -> None:
+        """
+        Check if the UI should appear and make it appear if it should.
+        """
+        line = self.ser.readline().decode('utf-8').strip() # Check if anything has been printed
+        if line == "BABBA":
+            if self.ui is None:
+                ui = Ui()         # now create the UI
+                ui.ShowUi()       # and show it
+        return None
 
 #region Instructions
 # Don't forget to set the port to micro bit (interpreter(thonny))
@@ -17,10 +32,4 @@ ui = None
 # Run the file in the terminal using python3
 #endregion
 
-while True:
-    line = ser.readline().decode('utf-8').strip() # Check if anything has been printed 
-    if line == "BABBA":
-        if ui is None:
-            ui = Ui()         # now create the UI
-            ui.ShowUi()       # and show it
  
