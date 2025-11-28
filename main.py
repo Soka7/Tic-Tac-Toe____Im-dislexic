112c1de4336b65b8d1ba419307dcc7d47feda779
Yolked64 <leobodinleo@gmail.com>
2025-11-28 12:14:48 +0100

    fix
    added the way to solve l51 error in referee main

---
+++ main.py
@@ -0,0 +1,24 @@
+from microbit import *
+import radio
+import music
+from players import *
+from random import randint
+
+radio.on()
+radio.config(channel=83)
+
+Joueur = JoueurC("Joueur")
+Joueur.num = randint(0,10000)
+
+while True:
+    radio.send(str("Ping") + str(Joueur.num))
+    while True:
+        incoming = radio.receive()
+        if incoming:
+            display.scroll(incoming)
+            if incoming == "Call":
+                Joueur.GetCall()
+            elif incoming == "Fight !":
+                display.scroll(incoming)
+                Joueur.Play()
+        display.clear()
